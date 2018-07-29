04fedcd493d2cce0b8bc85907cf4b78a56cc2657
Milan Sacher <milan.lanlada@gmail.com>
2018-07-29 20:33:52 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -52,5 +52,11 @@ function removeFromCart(item) {
 }
 
 function placeOrder(cardNumber) {
-  var amount 
+  var amount = total();
+  if (!cardNumber) {
+    return "Sorry, we don't have a credit card on file for you.";
+  } else {
+    cart = [];
+    return `Your total cost is $${amount}, which will be charged to the card ${cardNumber}.`;
+  }
 }
